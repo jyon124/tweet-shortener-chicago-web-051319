a7b94e492ffb14584b150569e6e3bee71dc39172
Jooyon Won <jyon1204@gmail.com>
2019-05-08 03:27:51 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,3 +1,17 @@
+def dictionary
+  dictionary = {
+    "too" => "2".
+    "to" => "2",
+    "two" => "2",
+    "four" => "4",
+    "for" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
 def word_substituter(tweet_one)
 
 end
