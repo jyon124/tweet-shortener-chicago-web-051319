b4fd9d13e78a70946691d16a6b4474f6a4b8fcd1
Jooyon Won <jyon1204@gmail.com>
2019-05-08 03:42:23 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -18,6 +18,10 @@ def bulk_tweet_shortener(tweets)
   end
 end
 
-def selective_tweet_shortener(tweets)
-
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end
 end
