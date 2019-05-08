646408fb66e50836fb90488dff769f0017535563
Jooyon Won <jyon1204@gmail.com>
2019-05-08 03:33:37 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,8 +2,8 @@ def dictionary
 dictionary = {"too" => "2", "to" => "2", "two" =>"2", "four" => "4", "for" => "4", "be" => "b", "you" => "u", "at" => "@", "and" => "&"}
 end
 
-def word_substituter(tweet_one)
-  tweet_one.split.collect do |word|
+def word_substituter(string)
+  string.split.collect do |word|
     if dictionary.keys.include?(word.downcase)
       word = dictionary[word.downcase]
     else
